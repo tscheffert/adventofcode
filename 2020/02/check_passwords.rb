31e2fef259690ac386b7a893591219c849ccc17f
Trent Scheffert <trent.scheffert@gmail.com>
2020-12-02 23:16:30 -0600

    Day 2, second puzzle done

--- 2020/02/check_passwords.rb
+++ 2020/02/check_passwords.rb
@@ -15,16 +15,18 @@ module CheckPasswords
 
     raise "Input file didn't have any password-with-policy lines" if password_with_policy_lines.empty?
 
-    validate_password(lines: password_with_policy_lines)
+    validate_passwords_for_sledding(lines: password_with_policy_lines)
+
+    validate_passwords_for_tobogganing(lines: password_with_policy_lines)
   end
 
-  FIRST_REGEX = /\A(?<minimum>\d{1,2})-(?<maximum>\d{1,2})\ (?<given_letter>[a-z]):\ (?<password>[a-z]+?)\z/
+  SLEDDING_REGEX = /\A(?<minimum>\d{1,2})-(?<maximum>\d{1,2})\ (?<given_letter>[a-z]):\ (?<password>[a-z]+?)\z/
 
-  def self.validate_password(lines:)
+  def self.validate_passwords_for_sledding(lines:)
     puts "Found #{lines.length} password-with-policy lines"
     failed_to_match = []
     passwords_with_policies = lines.each_with_object([]) do |line, memo|
-      if (m = line.match(FIRST_REGEX))
+      if (m = line.match(SLEDDING_REGEX))
         memo << {
           minimum:      m.named_captures.fetch('minimum').to_i,
           maximum:      m.named_captures.fetch('maximum').to_i,
@@ -37,14 +39,12 @@ module CheckPasswords
     end
 
     if failed_to_match.present?
-      warn "Lines that didn't match:"
+      warn "Lines that didn't match for sledding:"
       failed_to_match.each do |failed_line|
         warn failed_line
       end
 
       exit 1
-    else
-      puts 'Every line matched'
     end
 
     valid_passwords = passwords_with_policies.select do |pwp|
@@ -53,7 +53,51 @@ module CheckPasswords
         given_letter_count <= pwp[:maximum]
     end
 
-    puts "#{valid_passwords.length} passwords are valid"
+    puts "#{valid_passwords.length} passwords are valid; for the sledding place down the street"
+  end
+
+  TOBOGGANING_REGEX = /\A(?<first_index>\d{1,2})-(?<second_index>\d{1,2})\ (?<given_letter>[a-z]):\ (?<password>[a-z]+?)\z/
+
+  def self.validate_passwords_for_tobogganing(lines:)
+    puts "Found #{lines.length} password-with-policy lines"
+    failed_to_match = []
+    passwords_with_policies = lines.each_with_object([]) do |line, memo|
+      if (m = line.match(TOBOGGANING_REGEX))
+        memo << {
+          first_index:  m.named_captures.fetch('first_index').to_i - 1, # No index 0
+          second_index: m.named_captures.fetch('second_index').to_i - 1, # No index 0
+          given_letter: m.named_captures.fetch('given_letter'),
+          password:     m.named_captures.fetch('password')
+        }
+      else
+        failed_to_match << line
+      end
+    end
+
+    if failed_to_match.present?
+      warn "Lines that didn't match for tobogganing:"
+      failed_to_match.each do |failed_line|
+        warn failed_line
+      end
+
+      exit 1
+    end
+
+    valid_passwords = passwords_with_policies.select do |pwp|
+      if pwp[:password].length < pwp[:second_index]
+        warn 'Found password-with-policy that has less characters than the second index:'
+        warn pwp
+
+        exit 1
+      end
+
+      first_index_valid = pwp[:password][pwp[:first_index]] == pwp[:given_letter]
+      second_index_valid = pwp[:password][pwp[:second_index]] == pwp[:given_letter]
+
+      first_index_valid ^ second_index_valid
+    end
+
+    puts "#{valid_passwords.length} passwords are valid; for the Official Toboggan Corporate Policy"
   end
 
 end
